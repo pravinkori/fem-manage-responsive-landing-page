f52e5c4984df163b10b14d836658586288e2e121
Pravin <pravinkori@protonmail.ch>
2022-08-17 12:39:29 +0530

    Carousel slider complete for all display sizes

--- main.js
+++ main.js
@@ -9,19 +9,14 @@ navToggle.addEventListener('click', () => {
   primaryHeader.toggleAttribute('data-overlay')
 });
 
-// const slider = new A11YSlider(document.querySelector(".slider"), {
-//   adaptiveHeight: false,
-//   dots: true,
-//   centerMode: true,
-//   arrows: false,
-//   responsive: {
-//     480: {
-//       dots: false, // dots enabled 1280px and up
-//     },
-//   },
-// });
-
-const slider = new A11YSlider(document.querySelector('.slider'), {
-  adaptiveHeight: true,
-  dots: false,
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: false,
+  dots: true,
+  centerMode: true,
+  arrows: false,
+  responsive: {
+    480: {
+      dots: false, // dots enabled 1280px and up
+    },
+  },
 });
